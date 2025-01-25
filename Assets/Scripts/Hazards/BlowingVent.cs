dd37d4a4702c053c7aac8ec73dda207673bc9ec7
BACONGUDEN <oliver@guden.lol>
2025-01-25 10:22:54 +0100

    update blowingvent from IMPULSE to FORCE

--- Assets/Scripts/Hazards/BlowingVent.cs
+++ Assets/Scripts/Hazards/BlowingVent.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class BlowingVent : MonoBehaviour
 {
     [SerializeField]private Collider affectedTriggerArea;
-    [SerializeField,Range(0.1f, 10)] private float forceMagnitude = 10f;
+    [SerializeField,Range(0.1f, 30)] private float forceMagnitude = 10f;
     [SerializeField] string affectedTag = "Player";
 
     private void OnTriggerStay(Collider other)
@@ -12,7 +12,7 @@ public class BlowingVent : MonoBehaviour
         {
             Vector3 direction = other.transform.position - transform.position;
             direction.z = 0;
-            other.GetComponent<Rigidbody>().AddForce(direction.normalized * Vector3.Distance(other.transform.position, transform.position)* forceMagnitude, ForceMode.Impulse);
+            other.GetComponent<Rigidbody>().AddForce(direction.normalized * Vector3.Distance(other.transform.position, transform.position)* forceMagnitude, ForceMode.Force);
         }
     }
 }
