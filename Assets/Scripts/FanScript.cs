cb5aa0984edaf30788422945ff8a5d0b4b4f8c44
jonathan <jonathan.johansson@spinoffgames.se>
2025-01-25 14:33:25 +0100

    error fix
    error fix

--- Assets/Scripts/FanScript.cs
+++ Assets/Scripts/FanScript.cs
@@ -30,8 +30,8 @@ public class FanScript : MonoBehaviour
 
             if (targetRigidBody != null)
             {
-                var forceExert = Mathf.Max(maximumForce, forceMagnitude / ClickDistanceFromBubbleEdge);
-                forceExert = Mathf.Min(minimumForce, forceMagnitude / ClickDistanceFromBubbleEdge);
+                var forceExert = Mathf.Max(maximumForce, forceMagnitude / ClickDistanceFromBubbleCenter);
+                forceExert = Mathf.Min(minimumForce, forceMagnitude / ClickDistanceFromBubbleCenter);
                 forceDirection.z = 0;
                 force = -forceDirection.normalized * forceExert;
                 Debug.Log("Force: " + force);
