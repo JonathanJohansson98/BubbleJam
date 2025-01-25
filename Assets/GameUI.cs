c253113d4a81783ddb5676714fe5d88a7ad343ad
BACONGUDEN <oliver@guden.lol>
2025-01-25 13:40:01 +0100

    commit cursor based on playerAlive state

--- Assets/GameUI.cs
+++ Assets/GameUI.cs
@@ -1,14 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class GameUI : MonoBehaviour
 {
     [SerializeField] Image fanImage;
+    public FanScript fanScript;
 
     private void Update()
     {
-        fanImage.transform.position = Input.mousePosition;
+        //fanImage.transform.position = Input.mousePosition;
+        
+        if (fanScript.playerAlive == true)
+        {
+            fanImage.enabled = true;
+            fanImage.transform.position = Input.mousePosition;
+            Cursor.visible = false;
+        }
+        else
+        {
+            fanImage.enabled = false;
+            Cursor.visible = true;
+        }
+        
     }
 }
