9d82e69b0e50ce446d4a56bf83817c0dc177cfaa
Schismatron <89341100+Schismatron@users.noreply.github.com>
2025-01-25 13:14:10 +0100

    basic ahh menu pull request please approve master dongler

---
+++ Assets/MainMenuScript.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuScript : MonoBehaviour
+{
+    [SerializeField] private Button startGame;
+    [SerializeField] private Button exitGame;
+    [SerializeField] private Button options;
+    [SerializeField] private Button credits;
+
+
+    private void Awake()
+    {
+        startGame.onClick.AddListener(StartGame);
+        exitGame.onClick.AddListener(ExitGame);
+        options.onClick.AddListener(Options);
+        credits.onClick.AddListener(Credits);
+    }
+
+    private void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+
+    public void Options()
+    {
+        Debug.LogError("Options not implemented");
+    }
+    public void Credits()
+    {
+        Debug.LogError("Credits not implemented");
+    }
+}
