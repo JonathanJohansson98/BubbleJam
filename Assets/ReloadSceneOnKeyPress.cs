321dd1c6fdf18a13cf6ca1968e3eb7f33ca75fbf
BACONGUDEN <oliver@guden.lol>
2025-01-26 13:16:01 +0100

    18 naked cowboys at the shower at ram ranch

--- Assets/ReloadSceneOnKeyPress.cs
+++ Assets/ReloadSceneOnKeyPress.cs
@@ -3,6 +3,15 @@ using UnityEngine.SceneManagement;
 
 public class ReloadSceneOnKeyPress : MonoBehaviour
 {
+    void Start()
+    {
+        Collider collider = GetComponent<Collider>();
+        if (collider != null)
+        {
+            collider.isTrigger = true;
+        }
+    }
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.P))
@@ -15,5 +24,13 @@ public class ReloadSceneOnKeyPress : MonoBehaviour
     {
         Scene currentScene = SceneManager.GetActiveScene();
         SceneManager.LoadScene(currentScene.name);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            ReloadScene();
+        }
     }
 }
